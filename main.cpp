755e876f03bd49fbdbfe71287ba4446fe59dd3f4
AkhilKumar1601 <akhilchaudhary1601@gmail.com>
2026-05-08 16:45:31 +0530

    practiced basic c++

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+	cout << "Hello ji ji" << endl;
+	int i = 24;
+	char ch = 'a';
+	bool isTrue = 1;
+	float n = 2.1;
+	double n2 = 2.22;
+	cout << sizeof(i) << endl;
+	cout << sizeof(ch) << endl;
+	cout << sizeof(isTrue) << endl;
+	cout << sizeof(n) << endl;
+	cout << sizeof(n2) << endl;
+	char ch2 = 'b';
+	int noOfCh = ch2;
+	cout << noOfCh << endl;
+}
