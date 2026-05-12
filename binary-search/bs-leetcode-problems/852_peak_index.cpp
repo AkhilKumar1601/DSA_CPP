a80ae13e121ca88feccd9c14fa0af8f311780b79
AkhilKumar1601 <akhilchaudhary1601@gmail.com>
2026-05-12 19:03:02 +0530

    Added Leetcode problem 852 solution

---
+++ binary-search/bs-leetcode-problems/852_peak_index.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int peakIndexInMountainArray(vector<int>& arr) {
+        int s = 1;
+        int e = arr.size() - 2;
+        int mid = s + (e-s)/2;
+
+        while ( s <= e ) {
+            if ( arr[mid] > arr[mid - 1] && arr[mid] > arr[mid + 1]) {
+                return mid;
+            }
+            else if ( arr[mid] < arr[mid + 1] ) s = mid + 1;
+            else e = mid - 1;
+            mid = s + (e-s)/2;
+        }
+
+        return 0;
+    }
+};
