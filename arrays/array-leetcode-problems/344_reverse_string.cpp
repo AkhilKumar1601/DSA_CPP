2d9ff987883d97b1703c10e4e1dee9485dc45260
AkhilKumar1601 <akhilchaudhary1601@gmail.com>
2026-06-19 10:39:09 +0530

    Solved some leetcode questions

---
+++ arrays/array-leetcode-problems/344_reverse_string.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    void reverseString(vector<char>& arr) {
+        int s = 0;
+        int e = arr.size() - 1;
+        while (s < e) {
+            swap(arr[s++],arr[e--]);
+        }
+
+        return;
+    }
+};
