fb2d0cc33bebb683196413d2c39bc2eadacdf07f
AkhilKumar1601 <akhilchaudhary1601@gmail.com>
2026-08-04 22:05:58 +0530

    Perform linear search

--- 2d-array/tat.cpp
+++ 2d-array/tat.cpp
@@ -2,39 +2,33 @@
 #include <vector>
 using namespace std;
 
+bool searchMatrix(vector<vector<int>>& arr, int target) {
+  int rows = arr.size();
+  int cols = arr[0].size();
+
+  for (int i = 0; i < rows; i++) {
+    for (int j = 0; j < cols; j++) {
+      if (arr[i][j] == target) {
+        cout << "Found at Row = " << i << ", Column = " << j << endl;
+        return true;
+      }
+    }
+  }
+
+  return false;
+}
+
 int main() {
 
-  vector<vector<int>> arr(3,vector<int>(3));
+  vector<vector<int>> arr = {
+    {1,2,3},
+    {4,5,6},
+    {7,8,9}
+  };
 
-  cout << "Enter 9 elements:\n";
+  int target = 5;
 
-    // Input
-    for (int i = 0; i < arr.size(); i++) {
-        for (int j = 0; j < arr[i].size(); j++) {
-            cin >> arr[i][j];
-        }
-    }
-
-    cout << "\nRow-wise:\n";
-
-    // Row-wise
-    for (int i = 0; i < arr.size(); i++) {
-        for (int j = 0; j < arr[i].size(); j++) {
-            cout << arr[i][j] << " ";
-        }
-        cout << endl;
-    }
-
-    cout << "\nColumn-wise:\n";
-
-    // Column-wise
-    for (int j = 0; j < arr[0].size(); j++) {
-        for (int i = 0; i < arr.size(); i++) {
-            cout << arr[i][j] << " ";
-        }
-        cout << endl;
-    }
-
+  if(!searchMatrix(arr,target)) cout << "Not found";
   
   return 0;
 }
