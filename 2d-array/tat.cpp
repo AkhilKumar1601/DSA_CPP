a62008e028a150dc0df4d83f30e0cecc3c2d4a31
AkhilKumar1601 <akhilchaudhary1601@gmail.com>
2026-08-04 22:46:09 +0530

    sum row wise and col wise

--- 2d-array/tat.cpp
+++ 2d-array/tat.cpp
@@ -18,6 +18,36 @@ bool searchMatrix(vector<vector<int>>& arr, int target) {
   return false;
 }
 
+void rowWiseSum(vector<vector<int>>& arr) {
+  int rows = arr.size();
+  int cols = arr[0].size();
+
+  for ( int i = 0; i < rows; i++ ) {
+    int sum = 0;
+    for ( int j = 0; j < cols; j++ ) {
+      sum += arr[i][j];
+    }
+    cout << "Sum of " << i << "th row" << sum << endl;
+  }
+
+  return;
+}
+
+void colWiseSum(vector<vector<int>>& arr) {
+  int rows = arr.size();
+  int cols = arr[0].size();
+
+  for ( int j = 0; j < cols; j++ ) {
+    int sum = 0;
+    for ( int i = 0; i < rows; i++ ) {
+      sum += arr[i][j];
+    }
+    cout << "Sum of " << j << "th col" << sum << endl;
+  }
+
+  return;
+}
+
 int main() {
 
   vector<vector<int>> arr = {
@@ -28,7 +58,10 @@ int main() {
 
   int target = 5;
 
-  if(!searchMatrix(arr,target)) cout << "Not found";
-  
+  //if(!searchMatrix(arr,target)) cout << "Not found";
+  rowWiseSum(arr);
+  colWiseSum(arr);  
+
+
   return 0;
 }
