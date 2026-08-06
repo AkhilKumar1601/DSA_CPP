3e9fd5b35240744a4adc010f7cbbf66c90cf0e47
AkhilKumar1601 <akhilchaudhary1601@gmail.com>
2026-08-06 12:03:43 -0500

    Seive theorem

---
+++ basic-maths/count_primes_seive_theorem_204.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+
+    int countPrimes(int n) {
+
+        if (n <= 2)
+            return 0;
+
+        vector<bool> isPrime(n, true);
+
+        isPrime[0] = false;
+        isPrime[1] = false;
+
+        for (int i = 2; i < n; i++) {
+
+            if (isPrime[i]) {
+
+                for (int j = 2 * i; j < n; j += i) {
+                    isPrime[j] = false;
+                }
+            }
+        }
+
+        int count = 0;
+
+        for (int i = 2; i < n; i++) {
+            if (isPrime[i])
+                count++;
+        }
+
+        return count;
+    }
+};
