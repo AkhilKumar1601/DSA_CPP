53fe38f4fca4bc4b65ffb07857b06cb59a2b4a85
AkhilKumar1601 <akhilchaudhary1601@gmail.com>
2026-08-19 08:10:12 +0530

    MCQ questions

--- pointers/dpWithFun.cpp
+++ pointers/dpWithFun.cpp
@@ -26,7 +26,7 @@ int main() {
 
   cout << *(p - 1) << endl;*/
 
-  int arr[5] = {10,20,30,40,50};
+  //int arr[5] = {10,20,30,40,50};
 
   /*cout << arr << endl;
 
@@ -37,8 +37,51 @@ int main() {
   /*cout << *(arr+2) << endl;
   cout << *(&arr[0] + 2) << endl;*/
 
-  cout << *(arr + 1) << endl;
-  cout << *(&arr + 1) << endl;
+  // cout << *(arr + 1) << endl;
+  // cout << *(&arr + 1) << endl;
+
+  /* char ch[7] = "Hello";
+
+  cout << ch << endl;
+  cout << ch + 2 << endl;
+  cout << *(ch + 2) << endl; */
+
+  /*char ch[] = "ABCDE";
+
+  char *p = ch;
+
+  cout << p << endl;
+  cout << p + 2 << endl;
+  cout << *p << endl;
+  cout << *(p+3) << endl; */
+
+  /*int x = 10;
+  int *p = &x;
+
+  int **p2 = &p;
+
+  cout << x << endl;
+  cout << *p << endl;
+  cout << **p2 << endl;*/
+
+  /*int x = 10;
+  int *p = &x;
+  int **p2 = &p;
+  **p2 = 50;
+  cout << x << endl;
+  cout << *p << endl;
+  cout << **p2 << endl; */
+
+  int arr[4] = {10, 20, 30, 40};
+  int *p = arr;
+  int **p2 = &p;
+  cout << *p << endl;
+  p = p + 2;
+  cout << *p << endl;
+  cout << **p2 << endl;
+  **p2 = 100;
+  cout << arr[2] << endl;
+
 
   return 0;
 }
